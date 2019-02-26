cbe692b368edef11cb536dcb76c0e7a6db6c7a65
Chuck <washinima99@gmail.com>
2019-02-26 11:55:43 +0000

    Dual Projects
    Will delete the other one soon. Just need to put it working on the main one

---
+++ PinballDepthTest/Pinball Depth Test/Assets/KillOnTouch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnTouch : MonoBehaviour
+{
+    public GameObject parent;
+
+    private Vector3 targetScale;
+    private Vector3 currentScale;
+
+    public void Start()
+    {
+        targetScale = parent.transform.localScale;
+        currentScale = targetScale;
+    }
+
+    public void Update()
+    {
+        currentScale = Vector3.Lerp(
+                        currentScale,
+                        targetScale,
+                        0.1f);
+
+        parent.transform.localScale = currentScale;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+
+            targetScale += new Vector3(0.5f, 0, 0.5f);
+        }
+    }
+}
